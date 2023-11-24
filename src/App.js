82fb329cf855dad8628b4fd2e3bd79792d5be708
Kanya <zembekanya@gmail.com>
2023-11-24 16:50:50 +0200

    button and fetch inits

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import { Card, Heading, Text, Divider, Button } from "./Components/Card.styled";
 import GlobalStyle from "./Components/GlobalStyle";
 import PatternDivider from "./images/pattern-divider-desktop.svg";
-import ButtonIcon from "./images/icon-dice.svg"
+import ButtonIcon from "./images/icon-dice.svg";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  return (
-    <>
-      <GlobalStyle />
-      <Card>
-        <Heading>ADVICE #<span id="advice-id"></span></Heading>
-        <Text>"<span id="advice-text"></span>"</Text>
-        <Divider><img src={PatternDivider} alt="Divider" /></Divider>
-        <Button>
-          <img src={ButtonIcon} alt="Icon" />
-        </Button>
-      </Card>
-    </>
-  );
+
+  const [advice, setAdvice] = useState('')
+
+  let isMounted = true
+  const fetchAdvice = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json()
+
+    if (isMounted) {
+      setAdvice(data)
+      console.log(data)
+    }
+    useEffect(() => {
+      fetchAdvice();
+    }, []);
+
+    return (
+      <>
+        <GlobalStyle />
+        <Card>
+          <Heading>ADVICE #{advice.slip.id}</Heading>
+          <Text>"{advice.slip.advice}"</Text>
+          <Divider><img src={PatternDivider} alt="Divider" /></Divider>
+          <Button onClick={fetchAdvice}>
+            <img src={ButtonIcon} alt="Icon" />
+          </Button>
+        </Card>
+      </>
+    );
+  }
 }
 
 export default App;
