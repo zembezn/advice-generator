e4b1de737f3b46b2b1d772b9af1be4393b953ecf
Kanya <zembekanya@gmail.com>
2023-11-29 15:59:28 +0200

    eslint install

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    'env': {
+        'browser': true,
+        'es2021': true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended'
     ],
-    "overrides": [
+    'overrides': [
         {
-            "env": {
-                "node": true
+            'env': {
+                'node': true
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
+            'files': [
+                '.eslintrc.{js,cjs}'
             ],
-            "parserOptions": {
-                "sourceType": "script"
+            'parserOptions': {
+                'sourceType': 'script'
             }
         }
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
     },
-    "plugins": [
-        "react"
+    'plugins': [
+        'react'
     ],
-    "rules": {
+    'rules': {
+        'indent': ['error', 2],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'always'],
+        'no-console': 'warn'
     }
-}
+};
